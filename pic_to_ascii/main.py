4cf702b4f668f20a9aa545a878479f61de5023d5
Tom Gray <tomg77@yahoo.co.uk>
2016-11-15 09:58:04 +0000

    Added option to invert colours

--- pic_to_ascii/main.py
+++ pic_to_ascii/main.py
@@ -13,16 +13,18 @@ def main(args):
     parser.add_argument("picture", action="store", type=str)
     parser.add_argument("--width",type=int,action="store",default=80)
     parser.add_argument("--font_ratio",type=float,action="store",default=0.5)
+    parser.add_argument("--invert",action="store_const",const=True,default=False)
 
     results = parser.parse_args(args)
 
-    convert(PIL.Image.open(results.picture),results.width,results.font_ratio)
+    convert(PIL.Image.open(results.picture),results.width,results.font_ratio,
+            results.invert)
 
 def get_brightness(rgb):
     #Relative luminance - https://en.wikipedia.org/wiki/Relative_luminance
     return (0.2126*rgb[0]+0.7152*rgb[1]+0.0722*rgb[2])/255
 
-def convert(img,width=80,font_aspect_ratio=0.5):
+def convert(img,width=80,font_aspect_ratio=0.5,invert=False):
     """
     Takes a PIL image object and converts it to ASCII text with a
     default width of 80 characters
@@ -31,13 +33,17 @@ def convert(img,width=80,font_aspect_ratio=0.5):
     sf = img.width//width
     img = img.resize((round(img.width//sf/font_aspect_ratio),img.height//sf))
 
-    chars = [" ",",","^","#","$"]
+    chars = [" ",",","I","O","^","#","$"]
 
     for y in range(img.height):
         out.append([])
         for x in range(img.width):
+            if invert:
+                lightness = 1-get_brightness(img.getpixel((x,y)))
+            else:
+                lightness = get_brightness(img.getpixel((x,y)))
             out[-1].append(chars[round(
-                get_brightness(img.getpixel((x,y)))*len(chars))-1])
+                lightness*len(chars))-1])
 
     for x in out:
         for y in x:
