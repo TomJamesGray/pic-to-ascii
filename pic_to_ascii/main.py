d38ffd1a7006a6f9f2bd2f767b0ddfc33db98a8c
Tom Gray <tomg77@yahoo.co.uk>
2016-11-15 09:41:06 +0000

    Added arguments to specifiy font aspect ratios and the width

--- pic_to_ascii/main.py
+++ pic_to_ascii/main.py
@@ -11,24 +11,26 @@ def main(args):
 
     parser = argparse.ArgumentParser(description="Converts pictures to ASCII")
     parser.add_argument("picture", action="store", type=str)
+    parser.add_argument("--width",type=int,action="store",default=80)
+    parser.add_argument("--font_ratio",type=float,action="store",default=0.5)
 
     results = parser.parse_args(args)
 
-    convert(PIL.Image.open(results.picture))
+    convert(PIL.Image.open(results.picture),results.width,results.font_ratio)
+    print(results.font_ratio)
 
 def get_brightness(rgb):
     #Relative luminance - https://en.wikipedia.org/wiki/Relative_luminance
     return (0.2126*rgb[0]+0.7152*rgb[1]+0.0722*rgb[2])/255
 
-def convert(img,width=80):
+def convert(img,width=80,font_aspect_ratio=0.5):
     """
     Takes a PIL image object and converts it to ASCII text with a
     default width of 80 characters
     """
     out = []
     sf = img.width//width
-    font_aspect_ratio = 0.5
-    img = img.resize((round(img.width//sf/0.5),img.height//sf))
+    img = img.resize((round(img.width//sf/font_aspect_ratio),img.height//sf))
 
     chars = [" ",",","^","#","$"]
 
